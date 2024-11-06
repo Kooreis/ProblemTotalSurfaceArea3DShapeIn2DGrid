1e659a9fba60e57a48f3b9d657582aac2fec16b4
Kooreis <kooreis.official@gmail.com>
2024-11-06 10:22:37 -0600

    Implements a nested loop to iterate over each cell in the grid. If the cell value is greater than 0, it calculates the surface area of the cell by multiplying the cell value by 4 and adding 2, then adds this to the total surface area.

--- solution.cs
+++ solution.cs
@@ -1,4 +1,6 @@
-function surfaceArea(grid) {
-    let area = 0;
-    let n = grid.length;
+for(let i=0; i<n; i++) {
+        for(let j=0; j<n; j++) {
+            let cell = grid[i][j];
+            if(cell > 0) {
+                area += cell * 4 + 2;
 }
